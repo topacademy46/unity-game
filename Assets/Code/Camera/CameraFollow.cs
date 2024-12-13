8d6f5e498ad467c99b521d7661314528624c21ed
topacademy46 <topacademy46@gmail.com>
2024-12-13 17:10:44 +0300

    Add smooth property to CameraFollow.cs

--- Assets/Code/Camera/CameraFollow.cs
+++ Assets/Code/Camera/CameraFollow.cs
@@ -5,10 +5,12 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Transform targetTransform;
     [SerializeField] private float xOffset;
     [SerializeField] private float yOffset;
+    [SerializeField] private float smooth;
 
 
-    private void LateUpdate() {
-        transform.position = new Vector3(targetTransform.position.x + xOffset, targetTransform.position.y + yOffset, -10);
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(targetTransform.position.x + xOffset, targetTransform.position.y + yOffset, -10), smooth * Time.deltaTime);
     }
 
 
