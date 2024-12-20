901f72ad714db52425cfed45af44922070fce3c5
topacademy46 <topacademy46@gmail.com>
2024-12-20 16:06:07 +0300

    20.12

--- Assets/Code/Hero/Movement.cs
+++ Assets/Code/Hero/Movement.cs
@@ -5,40 +5,14 @@ public class Movement : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float jumpForce;
-    private AnimController animController;
 
     void Start()
     {
-        animController = GetComponent<AnimController>();
+
     }
 
     void Update()
     {
-        if (Mathf.Abs(rb.velocity.x) < 0.1f && Mathf.Abs(rb.velocity.y) < 0.1f)
-        {
-            animController.isIdle = true;
-        }
-        else
-        {
-            animController.isIdle = false;
-        }
-
-        if (rb.velocity.y > 0)
-        {
-            animController.isJumping = true;
-            animController.isFalling = false;
-        }
-        else if (rb.velocity.y < 0)
-        {
-            animController.isFalling = true;
-            animController.isJumping = false;
-        }
-        else
-        {
-            animController.isJumping = false;
-            animController.isFalling = false;
-        }
-
         Move();
         Jump();
     }
@@ -47,9 +21,6 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
         {
-            animController.isRun = true;
-            animController.isIdle = false;
-
             if (Input.GetKey(KeyCode.D))
             {
                 transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
@@ -60,10 +31,6 @@ public class Movement : MonoBehaviour
                 transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1, transform.localScale.y, transform.localScale.z);
                 transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
             }
-        }
-        else
-        {
-            animController.isRun = false;
         }
     }
 
